023af196c9b56bb03fe04ac4058aee855387825e
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-07-24 22:09:19 -0700

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,6 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-
 // listen for custom event
 socket.on('newMessage', function (message) {
   console.log('New Message', message);
--- server/server.js
+++ server/server.js
@@ -15,13 +15,33 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connection');
 
+    // emit to user that joined, from admin, text welcome chat
+    // broadcast to sockets that new user joined from admin
+  socket.emit('newMessage', {
+    from: 'chatAppAdmin',
+    text: 'Welcome to the chat app!'
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'chatAppAdmin',
+    text: 'New User joined the room',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log(message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     })
+
+    //  reference for challenge.
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
